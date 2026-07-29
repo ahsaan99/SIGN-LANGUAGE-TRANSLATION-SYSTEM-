7f3b55516f66a95e11d3666d0f2e9c4d64a318fc
Ahsaan <164712814+ahsaan99@users.noreply.github.com>
2026-07-29 17:17:46 +0400

    Update main copy.py

--- main copy.py
+++ main copy.py
@@ -11,7 +11,7 @@ import pyttsx3
 
 
 
-# ── MediaPipe setup s  ──────────────────────────────────────────────
+# ── MediaPipe setup   ──────────────────────────────────────────────
 mp_hands = mp.solutions.hands
 mp_draw  = mp.solutions.drawing_utils
 mp_styles = mp.solutions.drawing_styles
